39a7a5dcd3f8edc34d1d1bafe3d9bee04273a875
x <karolina_bp@hotmail.com>
2022-02-22 17:57:57 -0300

    feat: Adicionada a página sobre

--- PrevisaoTempo/PrevisaoTempo/Controllers/HomeController.cs
+++ PrevisaoTempo/PrevisaoTempo/Controllers/HomeController.cs
@@ -48,15 +48,9 @@ namespace PrevisaoTempo.Controllers
 
 
 
-        public IActionResult Privacy()
+        public IActionResult Sobre()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
